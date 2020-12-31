ed8908049aeb44b1f550775bc6199a412e0fb1f9
Tadele01 <tadeleyednkachw@gmail.com>
2020-12-31 05:22:11 -0800

    sale by match hacker rank question

---
+++ week-01/sale_by_match.py
@@ -0,0 +1,17 @@
+def sale_by_match(l, length):
+    hash_map = dict()
+    for i in range(length):
+        if l[i] in hash_map:
+            v = hash_map[l[i]]
+            v += 1
+            hash_map[l[i]] = v
+        else:
+            hash_map[l[i]] = 1
+    accumlator = 0
+    for key in hash_map:
+        if hash_map[key] > 1:
+            accumlator += hash_map[key] // 2
+    return accumlator
+l = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]
+length = 10
+print(sale_by_match(l, length))

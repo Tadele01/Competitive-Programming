a99a2fc7efd9d800b2f9222777d526569f33392f
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-04 22:50:51 -0800

    counting sort implemented but it does not working for negative numbers

---
+++ week-01/Day-02/counting_sort.py
@@ -0,0 +1,26 @@
+def counting_sort(arr):
+    '''
+        In counting sort we pick the max element from the array and build a max+1 range array
+        for storing how many times each element is repeated then from it we can build a sorted
+        version of our array 
+    '''
+    max_ = arr[0]
+    min_ = arr[0]
+    for elt in arr:
+        if elt > max_:
+            max_ = elt
+        if elt < min_:
+            min_ = elt
+    collector = []
+    for i in range(min_, max_+1):
+        collector.append(0)
+
+    for elt in arr:
+        collector[elt] +=1
+    sorted_arr = []
+    
+    for i in range(len(collector)):
+        for j in range(collector[i]):
+            sorted_arr.append(i)
+
+    return sorted_arr

945dc1c6dd1c3d3d1c84eac90db297ca5df4ba94
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-05 06:46:00 -0800

    counting sort updated

--- week-01/Day-02/counting_sort.py
+++ week-01/Day-02/counting_sort.py
@@ -10,48 +10,20 @@ def counting_sort(arr):
         when it comes to negative numbers my initial thought is to keep them in separate array
         
     '''
-    max_postive = arr[0]
-    min_postive = arr[0]
-    max_negative = -1
-    min_negative = -10000
+    min_ = arr[0]
+    max_ = arr[0]
     for elt in arr:
-        if elt >=0:
-            if elt > max_postive:
-                max_postive = elt
-            if elt < min_postive:
-                min_postive = elt
+        if elt < min_:
+            min_ = elt
+        if elt > max_:
+            max_ = elt
+    count_arr = [0] * (abs(min_)+abs(max_) + 1)
     for elt in arr:
-        if elt <0:
-            if elt > max_negative:
-                max_negative = elt
-            if elt < min_negative:
-                min_negative = elt
-    collector = []
-    negative_collector = []
-    min_negative = abs(min_negative)
-    max_negative = abs(max_negative)
-    for i in range(max_negative, min_negative):
-        negative_collector.append(0)
-    for i in range(min_postive, max_postive+1):
-        collector.append(0)
-    for elt in arr:
-        if elt < 0:
-            negative_collector[abs(elt)] +=1
-    negative_sorted = []
-    for i in range(len(negative_collector)):
-        for j in range(negative_collector[i]):
-            data = -1 * i
-            negative_sorted.append(data)
-    for elt in arr:
-        if elt >=0:
-            collector[elt] +=1
+        index = elt - min_
+        count_arr[index] +=1
     sorted_arr = []
-    
-    for i in range(len(collector)):
-        for j in range(collector[i]):
-            sorted_arr.append(i)
-    negative_sorted.extend(sorted_arr)
-    sorted_arr = negative_sorted
+    for i in range(len(count_arr)):
+        for j in range(count_arr[i]):
+            sorted_arr.append(i+min_)
     return sorted_arr
-
 print(counting_sort([-1, -1, -39, 0, 9]))

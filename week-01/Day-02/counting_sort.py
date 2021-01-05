7818943ce268eacb5ff1f6dc713f7221ed93bbd3
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-05 05:57:18 -0800

    negative numbers handled in counting sort

--- week-01/Day-02/counting_sort.py
+++ week-01/Day-02/counting_sort.py
@@ -6,24 +6,52 @@ def counting_sort(arr):
 
         its running time is O(n + k) where k is the larges number in the given array
         its space complexity is O(n) because we used additional memory
-    '''
-    max_ = arr[0]
-    min_ = arr[0]
-    for elt in arr:
-        if elt > max_:
-            max_ = elt
-        if elt < min_:
-            min_ = elt
-    collector = []
-    for i in range(min_, max_+1):
-        collector.append(0)
 
+        when it comes to negative numbers my initial thought is to keep them in separate array
+        
+    '''
+    max_postive = arr[0]
+    min_postive = arr[0]
+    max_negative = -1
+    min_negative = -10000
     for elt in arr:
-        collector[elt] +=1
+        if elt >=0:
+            if elt > max_postive:
+                max_postive = elt
+            if elt < min_postive:
+                min_postive = elt
+    for elt in arr:
+        if elt <0:
+            if elt > max_negative:
+                max_negative = elt
+            if elt < min_negative:
+                min_negative = elt
+    collector = []
+    negative_collector = []
+    min_negative = abs(min_negative)
+    max_negative = abs(max_negative)
+    for i in range(max_negative, min_negative):
+        negative_collector.append(0)
+    for i in range(min_postive, max_postive+1):
+        collector.append(0)
+    for elt in arr:
+        if elt < 0:
+            negative_collector[abs(elt)] +=1
+    negative_sorted = []
+    for i in range(len(negative_collector)):
+        for j in range(negative_collector[i]):
+            data = -1 * i
+            negative_sorted.append(data)
+    for elt in arr:
+        if elt >=0:
+            collector[elt] +=1
     sorted_arr = []
     
     for i in range(len(collector)):
         for j in range(collector[i]):
             sorted_arr.append(i)
-
+    negative_sorted.extend(sorted_arr)
+    sorted_arr = negative_sorted
     return sorted_arr
+
+print(counting_sort([-1, -1, -39, 0, 9]))

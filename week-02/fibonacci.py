2cd2687b96e48e8e3c4c47be9375336223491ca2
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-12 06:29:02 -0800

    fibonacci

--- week-02/fibonacci.py
+++ week-02/fibonacci.py
@@ -1,5 +1,5 @@
-# def fibonacci(n):
-#     if n == 1 or n == 0:
-#         return 1
-#     else:
-#         return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)

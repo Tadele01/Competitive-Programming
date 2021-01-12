9ecda7580aac8146760b102fa55cbc2246407532
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-12 06:41:32 -0800

    fibonacci using iterative approach

--- week-02/fibonacci.py
+++ week-02/fibonacci.py
@@ -3,3 +3,13 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
+
+
+def fib_iterative(n):
+    a, b = 0, 1
+    for i in range(n):
+        tmp = a
+        a = b
+        b = tmp + a
+    return b
+

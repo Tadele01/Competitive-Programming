a4176631376afa00041eddc56df58631572bcee3
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-13 03:03:19 -0800

    implement intersection of two arrays using set type

--- week-02/intersection_of_array.py
+++ week-02/intersection_of_array.py
@@ -1,6 +1,13 @@
 def intersection(arr1, arr2):
-    inter_section = []
+    '''inter_section = []
     for elt in arr1:
         if elt in arr2 and elt not in inter_section:
             inter_section.append(elt)
+    return inter_section'''
+
+    #alternative approch by using python built in set class
+
+    set1 = set(arr1)
+    set2 = set(arr2)
+    inter_section = set1.intersection(set2)
     return inter_section

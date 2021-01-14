6c5b015ed28a59b569f73589a6174339545d55bf
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-14 02:20:50 -0800

    reversing a linked list

---
+++ week-01/Day-07/reversing_linked_list.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+def reverseList(head):
+    #reversing linked list using iteration
+
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+    if arr:
+        head_val =arr.pop()
+        node = ListNode(head_val)
+        head = node
+        while len(arr) != 0:
+            cur = arr.pop()
+            cur_node = ListNode(cur)
+            node.next = cur_node
+            node = cur_node
+        return head

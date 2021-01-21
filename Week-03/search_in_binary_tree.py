6d23428440a9c5a24ca9dfbdef1802ba8bb149dd
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-21 07:19:48 -0800

    search in binary tree:

---
+++ Week-03/search_in_binary_tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        found = self.root_finder(root, val)
+        if found:
+            sub_array = []
+            data = self.traverse(found, sub_array)[0]
+            return data
+        return None
+    def root_finder(self, root, val):
+        while root:
+            if root.val == val:
+                return root
+            elif root.val >= val:
+                return self.root_finder(root.left, val)
+            elif root.val < val:
+                return self.root_finder(root.right, val)
+        return None
+    def traverse(self, root, sub_tree):
+        if root:
+            sub_tree.append(root)
+            self.traverse(root.left, sub_tree)
+            self.traverse(root.right, sub_tree)
+        return sub_tree

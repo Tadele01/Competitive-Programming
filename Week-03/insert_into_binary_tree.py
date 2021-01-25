b2e8102540a1585d48a32c316dabecabbf176ae0
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-25 03:47:42 -0800

    insert into binary tree optimized

--- Week-03/insert_into_binary_tree.py
+++ Week-03/insert_into_binary_tree.py
@@ -8,25 +8,12 @@ class TreeNode:
 class Solution:
     def insertIntoBST(self, root:TreeNode, val:int) -> TreeNode:
         if root is None:
-            new_node = TreeNode(val=val)
-            return new_node
-        self.insert(root, val)
+            return TreeNode(val)
+        elif root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif root.val < val:
+            root.right = self.insertIntoBST(root.right,val)
         return root
-    
-    def insert(self, root, val):
-        if root:
-            if root.val > val:
-                next_node = root.left
-                if next_node is None:
-                    new_node = TreeNode(val = val)
-                    root.left = new_node
-                    return
-                self.insert(root.left, val)
-            if root.val < val:
-                next_node = root.right
-                if next_node is None:
-                    new_node = TreeNode(val = val)
-                    root.right = new_node
-                    return
-                self.insert(root.right, val)
+        
+        
     

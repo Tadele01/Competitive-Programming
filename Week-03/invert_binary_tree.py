3d35519ea0bf310e86117adcbebdb79dee3c5bfd
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-24 22:41:22 -0800

    inverting binary tree optimized

--- Week-03/invert_binary_tree.py
+++ Week-03/invert_binary_tree.py
@@ -1,3 +1,9 @@
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         '''
@@ -8,16 +14,11 @@ class Solution:
                 
                 for every right and left do the same
         ''' 
-        arr = [root]
-        self.invert_helper(root, arr)
-        return arr[0]
-    def invert_helper(self, root, arr):
         if root:
             if root.left is None and root.right is None:
-                return 
+                return root
             else:
                 root.left, root.right = root.right, root.left
-                self.invert_helper(root.left, arr)
-                self.invert_helper(root.right, arr)
-        else:
-            return
+                self.invertTree(root.left)
+                self.invertTree(root.right)
+        return root

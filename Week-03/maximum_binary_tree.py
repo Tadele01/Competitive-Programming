8c013a7f6ee554dc310350f7c37f5916ea2ca677
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-25 07:18:11 -0800

    max depth of binary tree using iterative

--- Week-03/maximum_binary_tree.py
+++ Week-03/maximum_binary_tree.py
@@ -11,3 +11,18 @@ class Solution:
         right = self.maxDepth(root.right)
         return max(left, right) + 1
         
+    def maxDepthIterative(self, root:TreeNode)->int:
+        if not root:
+            return 0    
+        max_height = 1
+        stack = [(root, 1)]
+        while stack:
+            root, depth = stack.pop()
+            if not root.left and not root.right:
+                max_height = max(max_height, depth)
+            if root.left:
+                stack.append((root.left, depth+1))
+            if root.right:
+                stack.append((root.right, depth+1))
+           
+        return max_height

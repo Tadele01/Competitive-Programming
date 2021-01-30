86c6853a6559631abae1932ac13c455e4685f2fe
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2021-01-30 07:15:26 -0800

    Update binary_search.py

--- Week-03/Binary_search/binary_search.py
+++ Week-03/Binary_search/binary_search.py
@@ -1,22 +1,15 @@
 class Solution:
-    def binary_search(self, arr, target, low=0, index=0):
-        low = 0
-        high = len(arr) - 1
-        mid = (low + high) // 2
-        index += mid
-        if arr:
-            if arr[mid] == target:
-                return index
-            elif arr[mid] > target:
-                index -= mid
-                arr = arr[:mid] 
-                return self.binary_search(arr, target, index=index)
-            elif arr[mid] < target:
-                arr = arr[mid+1:]
-                index += 1
-                return self.binary_search(arr, target, index=index)
-        return -1
-
-
-    def search(self, nums:list, target:int)->int:
-        return self.binary_search(nums, target)
+    def search(self, nums: List[int], target: int) -> int:
+        return self.binary_search(nums, target, 0, len(nums)-1)
+    
+    def binary_search(self, nums, target, left, right):
+        if left > right:
+            return -1
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            return self.binary_search(nums, target, left, mid-1)
+        else:
+            return self.binary_search(nums, target, mid+1, right)
+        

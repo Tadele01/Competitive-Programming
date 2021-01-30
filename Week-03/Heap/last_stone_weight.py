623d66a8911c74264a37563c94cde410fcb07938
Tadele01 <tadeleyednkachw@gmail.com>
2021-01-30 02:40:09 -0800

    last stone weight

---
+++ Week-03/Heap/last_stone_weight.py
@@ -0,0 +1,17 @@
+from heapq import heappop, heappush, heapify
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = []
+        for stone in stones:
+            max_heap.append(-1*stone)
+        heapify(max_heap)
+        while max_heap:
+            if len(max_heap) == 1:
+                return abs(max_heap[0])
+            first_max = abs(heappop(max_heap))
+            second_max = abs(heappop(max_heap))
+            if first_max == second_max:
+                continue
+            diff = first_max - second_max
+            heappush(max_heap, -1*diff)
+        return 0

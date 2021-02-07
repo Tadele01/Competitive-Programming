3ed84e3ab539363d3150b519e173b32e13670f7c
Tadele01 <tadeleyednkachw@gmail.com>
2021-02-06 23:02:23 -0800

    find first and last occurence of a number in as sorted array

---
+++ Week-03/Binary_search/find_first_and_last.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        first_last = []
+        left, right = 0, len(nums)- 1
+        n = len(nums)
+        self.binary_search(nums, first_last, n, left, right, target)
+        first_last.sort()
+        if first_last:
+            value = [first_last[0], first_last[-1]]
+        else:
+            value = [-1, -1]
+        return value
+    def binary_search(self, nums, first_last, n, left, right, target):
+        if(left <= right):
+            mid = left + (right - left)//2
+            if nums[mid] == target:
+                first_last.append(mid)
+                self.binary_search(nums, first_last, n, left, mid-1, target)
+                self.binary_search(nums, first_last, n, mid+1, right, target)
+            elif nums[mid] > target:
+                self.binary_search(nums, first_last, n, left, mid-1, target)
+            elif nums[mid] < target:
+                self.binary_search(nums, first_last, n, mid+1, right, target)        

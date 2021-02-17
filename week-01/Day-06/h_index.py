ae546f47e50f4df57208593fc1b12b606c9ea712
Tadele01 <tadeleyednkachw@gmail.com>
2021-02-17 12:10:37 +0300

    h-indexI  Nlogn solution

--- week-01/Day-06/h_index.py
+++ week-01/Day-06/h_index.py
@@ -1,21 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        if not citations:
-            return 0
-        h_index = 0
-        N = len(citations) 
-        for h in range(N+1):
-            count = 0
-            for value in citations:
-                if value >= h:
-                    count +=1
-            if count >= h:
-                count = 0
-                N_h = N - h
-                for value in citations:
-                    if value <= h:
-                        count +=1
-                if count >= N_h and h > h_index:
-                    h_index = h
+        citations = sorted(citations)
+        h_index = 0 
+        for i, cite in enumerate(citations):
+            if cite > i:
+                h_index +=1
         return h_index
-                    

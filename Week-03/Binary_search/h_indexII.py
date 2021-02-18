95be11fddeb286757543d7314723840f4f8b2666
Tadele01 <tadeleyednkachw@gmail.com>
2021-02-18 11:15:11 +0300

    h-indexII using binary search O(logn)

--- Week-03/Binary_search/h_indexII.py
+++ Week-03/Binary_search/h_indexII.py
@@ -1,10 +1,12 @@
 from typing import List
 class Solution:
     def h_indexII(self, citations:List)->int:
-        h_index = 0
-        index = 0
-        for i in range(len(citations)-1, -1, -1):
-            if citations[i] > index:
-                h_index +=1
-            index +=1
-        return h_index
+        left, right, N, h_index = 0, len(citations)-1, len(citations), 0
+        while left <= right:
+            mid = left + ((right - left)//2)
+	    if citations[mid] >= (N-mid):
+	        h_index = N-mid
+	        right = mid - 1
+	    else:
+	        left = mid + 1
+	return h_index        

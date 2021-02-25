0049fcc16549c27d6df23d1f8148a61510c98362
Tadele01 <tadeleyednkachw@gmail.com>
2021-02-25 15:50:21 +0300

    course schedule II

---
+++ week-04/graphs/Topological_sort/course_scheduleII.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        courses = set([i for i in range(numCourses)])
+        if not prerequisites:
+            return list(courses)
+        adj_list, result, indegree = defaultdict(list), list(), {}
+        for edge in prerequisites:
+            adj_list[edge[1]].append(edge[0])
+            if edge[1] in adj_list[edge[0]]:
+                return []
+            if edge[0] in indegree:
+                indegree[edge[0]] +=1
+            else:
+                indegree[edge[0]] = 1
+        queue = deque()
+        for i in range(numCourses):
+            if i not in indegree:
+                queue.append(i)
+                result.append(i)
+        while queue:
+            cur_course = queue.popleft()
+            for neighbour in adj_list[cur_course]:
+                indegree[neighbour] -= 1
+                if indegree[neighbour] ==0:
+                    indegree.pop(neighbour)
+                    queue.append(neighbour)
+                    if neighbour not in result:
+                        result.append(neighbour)
+                    courses.remove(neighbour)
+        if indegree:
+            return []
+        if len(result) < numCourses:
+            result.extend(list(courses))
+            return result
+        return result
+                    
+        
+        
+        

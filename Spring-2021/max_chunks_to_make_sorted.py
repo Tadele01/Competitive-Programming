a94aaabc978139e1721f22bc4a77c05a2c22d76d
Tadele01 <tadeleyednkachw@gmail.com>
2021-05-25 18:33:08 +0300

    maxchunks updated

--- Spring-2021/max_chunks_to_make_sorted.py
+++ Spring-2021/max_chunks_to_make_sorted.py
@@ -1,21 +1,17 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
+        container = set()
+        longest = 0
+        for i in range(len(arr)):
+            all_found = True
+            for k in range(0, i):
+                if k not in container:
+                    all_found = False
+                    
+            if all_found:
+                longest +=1
+            
+            container.add(arr[i])
         
-        chunk_starts = set([0])
-        self.chunk_creator(arr, 0, len(arr), chunk_starts)
-        
-        return len(chunk_starts)
-        
-    def chunk_creator(self, arr, start, end, chunk_starts):
-        
-        max_elt  = arr[start]
-        
-        for i in range(start, end):
-            if i < len(arr) and arr[i] > max_elt and max(arr[start:i]) < min(arr[i:end+1]):
-                chunk_starts.add(i)
-                self.chunk_creator(arr, start, i, chunk_starts)
-                self.chunk_creator(arr, i, end, chunk_starts)
-                break
-
-        
+        return longest
         

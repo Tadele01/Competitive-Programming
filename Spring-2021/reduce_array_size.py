caea7739fd98239da8599c5deecd206a68489025
Tadele01 <tadeleyednkachw@gmail.com>
2021-07-06 11:31:05 +0300

    reduce array size to half

---
+++ Spring-2021/reduce_array_size.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        count_arr = sorted(Counter(arr).values(), reverse=True)
+        n =  len(arr) / 2
+        running_sum, count = 0, 0
+        for freq in count_arr:
+            if running_sum >= n:
+                return count
+            running_sum += freq
+            count += 1
+        
+        return count

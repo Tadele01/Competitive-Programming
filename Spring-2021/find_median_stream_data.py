5329eb4f976aadbede93593e2d55ed36c3ecd8cb
Tadele01 <tadeleyednkachw@gmail.com>
2021-07-11 16:02:16 +0300

    find median from stream data

---
+++ Spring-2021/find_median_stream_data.py
@@ -0,0 +1,19 @@
+from heapq import heappop, heappush, heappushpop
+class MedianFinder:
+
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []        
+
+    def addNum(self, num: int) -> None:
+        if len(self.min_heap) == len(self.max_heap):
+            heappush(self.max_heap, -heappushpop(self.min_heap, num))
+        else:
+            heappush(self.min_heap, -heappushpop(self.max_heap, -num))
+        
+
+    def findMedian(self) -> float:
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] + (-self.max_heap[0])) / 2
+        else:
+            return -self.max_heap[0]

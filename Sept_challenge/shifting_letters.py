49c50a6b9ef0ca1c9020af14d4e100e693beae92
Tadele01 <tadeleyednkachw@gmail.com>
2021-09-13 20:58:42 +0300

    shifting letters

---
+++ Sept_challenge/shifting_letters.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def get_ascii(self, s_list):
+        new_arr = []
+        for value in s_list:
+            new_arr.append(ord(value))
+        return new_arr
+    def get_result(self, arr):
+        result = []
+        for value in arr:
+            if value > ord('z'):
+                diff = value - (ord('z')+1)
+                res = ord('a') + (diff % 26)
+                result.append(chr(res))
+            else:
+                result.append(chr(value))
+        return ''.join(result)
+    def shiftingLetters(self, S: str, shifts: List[int]) -> str:
+        s_list = list(S)
+        s_list_ascii = self.get_ascii(s_list)
+        shifts_sum = sum(shifts)
+        for i in range(len(s_list_ascii)):
+            s_list_ascii[i] += shifts_sum
+            shifts_sum -= shifts[i]
+        result = self.get_result(s_list_ascii)
+        return result
+        
